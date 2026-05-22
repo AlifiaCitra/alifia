dc4582bf74de42f23f596f6aea7546706d0e787c
AlifiaCitra <alifiacytra@gmail.com>
2026-05-22 14:24:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,4 @@ st.write(
 )
 st.title("Haii")
 st.title("_SEMANGATT_ YAA :blue[SAYANG] :sunglasses:")
-st.button("Reset", type="primary")
-if st.button("Say hello"):
-    st.write("Why hello there")
-else:
-    st.write("Goodbye")
 
-if st.button("Aloha", type="tertiary"):
-    st.write("Ciao")
