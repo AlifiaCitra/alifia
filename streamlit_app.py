5f7e50d8e3700c9ebcf2522fba0a08cc5f39b0c1
AlifiaCitra <alifiacytra@gmail.com>
2026-05-22 14:17:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.title("This is a title")
-st.title("_Streamlit_ is :blue[cool] :loveglasses:")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
